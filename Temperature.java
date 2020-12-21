f28eb0f2b6e043bffa7a097aef8bcb4d3fc6af37
IshanBhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2020-12-21 07:53:55 +0530

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Temperature 
 {
     public static void main()
     {
-        double fahrenheit = 98.6;
+        Scanner sc = new Scanner(System.in);
+        double fahrenheit = sc.nextDouble();
         double celsius = 5 * (fahrenheit-32) / 9;
         System.out.println("Temperature in celsius: "+celsius);
     }
