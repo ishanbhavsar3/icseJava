7f9353a39baf95f6623c7285f44bfa6fb780cbd3
IshanBhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2020-12-24 15:57:25 +0530

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Tables
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the number whose table you want.");
+        long a = sc.nextLong();
+        System.out.println("Input till what number you want the table.");
+        long b = sc.nextLong();
+        for(long c = 0; c<=b; c++)
+        {
+            System.out.println();
+            System.out.println(a+"*"+c+"="+(long)(a*c));
+        }
+    }
+}
