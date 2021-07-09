1dee7a19fa675eddd3a37f5c80fb69fe124f8902
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2021-07-09 19:17:39 +0530

    Add files via upload

---
+++ DigitSum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class DigitSum
+{
+    public static void main()
+    {
+        int i,sum;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number ");
+        i=sc.nextInt();
+        for(sum=0 ;i!=0 ;i/=10)
+        {
+            sum+=i%10;
+        }
+        System.out.println("Sum of digits "+sum);
+    }
+}
