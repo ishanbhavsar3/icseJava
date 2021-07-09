d79eb3a4f8cfc8b67cccd92ed4d09e744e845d2b
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2021-07-09 19:12:21 +0530

    Add files via upload

---
+++ LoopingIt.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class LoopingIt
+{
+    public static void main()  
+    {
+        int i=0, j=0 ,s=0; 
+        Scanner sc = new Scanner(System.in);
+        byte a = sc.nextByte();
+        switch(a)
+        {
+            case 1:
+            System.out.println("Enter a number");
+            int n = sc.nextInt();
+            while(i<n) 
+
+            {
+                System.out.print(j+" "); 
+                j = j + 3 + s;
+                i = i + 1; 
+                s = s + 2;
+            }
+            break;
+
+            case 2:
+            System.out.println("Enter a number");
+            int f = sc.nextInt();
+            for(i = 1; i<=f; i++)
+            {
+                System.out.print(((i*i) - 1) + " ");
+            }
+            break;
+            
+            case 3:
+            int b = sc.nextInt();
+            do
+            {
+                System.out.print(j+" "); 
+                j = j + 3 + s;
+                i = i + 1; 
+                s = s + 2;
+            } while(i<b);
+            break;
+            
+            default:
+            {
+                System.out.println("null");
+            }
+        }  
+    }
+}
