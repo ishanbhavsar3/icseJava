cf3b6bf05187ee1ad6d34c177c7ee167e6621dcd
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2021-12-17 12:39:32 +0530

    Update Filepath.java

--- StringFunctions/Filepath.java
+++ StringFunctions/Filepath.java
@@ -5,8 +5,11 @@ public class Filepath {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter filepath to get the folder path, file name & extension.");
         str = sc.next();
+        //storing the folder path in variable path
         path = str.substring(0, str.lastIndexOf("\\"));
+        //storing the file name using substring(), indexOf() & lastIndexOf() fns on str
         fileName = str.substring((char)(str.lastIndexOf("\\")+1), (str.indexOf(".")));
+        //storing the file type using indexOf() & length() fns on String str
         fileType = str.substring((char)(str.indexOf(".")+1), str.length());
 
         System.out.println("Path:       "+path);
