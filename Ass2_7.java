d21217300cb805306f95b9fda040ca12cf1c3215
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-22 20:57:35 +0530

    Delete Ass2_7.java

--- Ass2_7.java
+++
@@ -1,8 +0,0 @@
-public class Ass2_7
-{
-    public static void main(int a, int b)
-    {
-        int z = a > b ? a:b ;
-        System.out.println("The larger of the two numbers "+a+" and "+b+ " is "+z);
-    }
-}
