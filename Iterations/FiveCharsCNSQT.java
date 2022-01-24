46a197a73a8782611b45947575a39b088197cbbd
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-24 15:59:18 +0530

    minor change

--- Iterations/FiveCharsCNSQT.java
+++ Iterations/FiveCharsCNSQT.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
-
-public class FiveChars
+public class FiveCharsCNSQT
 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a character: ");
         char ch = in.next().charAt(0);
         System.out.println("Next 5 characters from " + ch + " are:");
-        for (int i = 1; i <= 5; i++)
-        {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(++ch);
         }
     }
