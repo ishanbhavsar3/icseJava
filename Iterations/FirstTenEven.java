f9a9a60a09b347b36afa2817c9b789486efffe52
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-24 15:52:48 +0530

    Update and rename FirstTenEven.java to Iterations/FirstTenEven.java

--- Iterations/FirstTenEven.java
+++ Iterations/FirstTenEven.java
@@ -1,11 +1,11 @@
 // 2 4 6 8 10 12 14 16 18 20 
 public class FirstTenEven
 {
-    public static void main()
+    public static void main(String[] args)
     {
         for(int i = 2; i<=20; i+=2)
         {
-                System.out.print(i+" ");
+            System.out.print(i+" ");
         }
     }
 }
