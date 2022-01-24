b6e65cded039a75edd1c4ac1117960b07687f43e
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-24 09:43:30 +0530

    Update DigitSum.java

--- DigitSum.java
+++ DigitSum.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-
 class DigitSum
 {
-    public static void main()
+    public static void main(String[] args)
     {
         int i,sum;
         Scanner sc=new Scanner(System.in);
