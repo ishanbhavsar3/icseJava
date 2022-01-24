92e254a602d4e67a1ad1aab5e2bd4288560e22da
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-24 15:54:39 +0530

    Update FirstTenOdd.java

--- FirstTenOdd.java
+++ FirstTenOdd.java
@@ -1,10 +1,11 @@
+// 1 3 5 7 9 11 13 15 17 19 
 public class FirstTenOdd
 {
-    public static void main()
+    public static void main(String[] args)
     {
         for(int i = 1; i<=19;i=i+2)
         {
-                System.out.println(i);
+            System.out.print(i+" ");
         }
     }
 }
