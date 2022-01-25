1a01979e2f7cbed61b981ce367c116bb9f3e0b49
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 15:05:23 +0530

    ...

--- ConditionalOperators/TriangleType.java
+++ ConditionalOperators/TriangleType.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class TriangleType
 {
     void checker(int a, int b, int c)
@@ -10,5 +11,18 @@ public class TriangleType
             System.out.println("The triangle is scalene.");
         else
             System.out.println("Invalid inputs");
+    }
+    public static void main(String[] args)
+    {
+        int one, two, three;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input three sides of triangle to get its type.")
+        
+        one = sc.nextInt();
+        two = sc.nextInt();
+        three = sc.nextInt();
+        
+        TriangleType call = new TriangleType();
+        call.checker(one, two, three);
     }
 }
