9671e8bf9f4f66d0515994f706078b5fa7f2da81
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 15:10:51 +0530

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,12 +1,13 @@
 public class Time
 {
-    public static void main()
+    public static void main(String[] args)
     {
+        // Question : Express 3750 seconds in hours, minutes and seconds.
         int seconds = 3750;
         int second = seconds % 60;
         int hour = seconds / 60;
         int minutes = hour % 60;
         hour = hour / 60;
-        System.out.print( hour + " hour " + minutes + " minutes " + second + " seconds ");
+        System.out.print( hour + " hours " + minutes + " minutes " + second + " seconds ");
     }
 }
