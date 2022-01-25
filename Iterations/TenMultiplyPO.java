123223624b0905376cfb1709bef8a20c1d901960
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 15:15:34 +0530

    Update and rename TenMultiply.java to Iterations/TenMultiplyPO.java

--- Iterations/TenMultiplyPO.java
+++ Iterations/TenMultiplyPO.java
@@ -1,7 +1,8 @@
+//Pattern : 1 11 111 1111 11111 111111 1111111 11111111 111111111 1111111111 ....
 import java.util.Scanner;
-public class TenMultiply
+public class TenMultiplyPO
 {
-    public static void main()
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of terms: ");
