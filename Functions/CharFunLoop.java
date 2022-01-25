c42d03c89cc68fcbdab612d0cc3dee3ebddc379f
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 16:20:11 +0530

    Update CharFunLoop.java

--- Functions/CharFunLoop.java
+++ Functions/CharFunLoop.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class CharFunLoop
 {
-    public static void main()
+    public static void main(String[] args)
     {
         for(int i = 1; i<=5; i++)
         {
