1af46ca8f0143b666a606c7495838794411ba0b0
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 16:22:34 +0530

    Update First30Reverse.java

--- Iterations/First30Reverse.java
+++ Iterations/First30Reverse.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class First30Reverse
 {
-    public static void main()
+    public static void main(String[] args)
     {
         for(int i = 30; i>=1; i--)
         {
