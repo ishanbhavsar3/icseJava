523530eed9670e2009ddf05f0794d8ced65a446a
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 16:17:26 +0530

    Update and rename Quadratic.java to Functions/MfuncQuadratic.java

--- Functions/MfuncQuadratic.java
+++ Functions/MfuncQuadratic.java
@@ -1,7 +1,7 @@
 import java.util.*;
-public class Quadratic
+public class MfuncQuadratic
 {
-    public static void main()
+    public static void main(String[] args)
     {
         System.out.println("Input a b and c of a Quadratic equations to find its roots.");
         Scanner obj = new Scanner(System.in);
