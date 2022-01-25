50abccf9522fab7dec62a4c43b38f38b5cd063bb
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 15:45:39 +0530

    Update and rename Multiple_Arrays.java to Arrays/Marks1.java

--- Arrays/Marks1.java
+++ Arrays/Marks1.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-
-public class Multiple_Arrays 
+public class Marks1
 {
-    public static void main() 
+    public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         int[] roll = new int[4];
