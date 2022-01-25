889c0a1fe2f076d9a21f952c082cf339765dd23e
Ishan Bhavsar <53740282+ishanbhavsar3@users.noreply.github.com>
2022-01-25 16:41:03 +0530

    Update and rename Series1.java to Iterations/Series1.java

--- Iterations/Series1.java
+++ Iterations/Series1.java
@@ -1,10 +1,11 @@
+// 1 12 123 1234 12345 123456 1234567 12345678 123456789 
 import java.util.Scanner;
 public class Series1
 {
-    public static void main()
+    public static void main(String[] args)
     {
             Scanner sc = new Scanner(System.in);
-            System.out.print("ENter numbers ");
+            System.out.print("Enter numbers for series.");
             int n = sc.nextInt();
             int s = 0, i;
             for(i = 1; i<=n; i++)
